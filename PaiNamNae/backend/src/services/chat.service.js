80f93b5656293b07ba83098aa4bdddbf0e1879ac
pheeratchai <pheeratchai.s@kkumail.com>
2026-02-17 15:52:43 +0700

    aong

---
+++ PaiNamNae/backend/src/services/chat.service.js
@@ -0,0 +1,14 @@
+// src/services/chat.service.js
+
+const getMessages = async (chatRoomId) => {
+  return [];
+};
+
+const sendMessage = async (data) => {
+  return data;
+};
+
+module.exports = {
+  getMessages,
+  sendMessage
+};
